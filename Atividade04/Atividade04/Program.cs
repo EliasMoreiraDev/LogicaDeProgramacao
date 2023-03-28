c376bc23faba634b63d03a40ee651d4682848322
2022103030076 <elias.moreiradosantos.28@gmail.com>
2023-03-28 11:08:28 -0400

    Atividade Calcula nota com switch case.

---
+++ Atividade04/Atividade04/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Aumento Salarial---\n");
+            Console.WriteLine("Cargos: [1]- gerente [2]- Engenheiro [3]-Técnico");
+
+            Console.Write("Informe seu cargo: ");
+            int cargo = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Informe seu salário: ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+
+            double salarioReajustado;
+
+            switch (cargo)
+            {
+                case 1:
+                    salarioReajustado = salario + (salario * 0.1);
+                    break;
+
+                case 2:
+                    salarioReajustado = salario + (salario * 0.2);
+                    break;
+
+                case 3:
+                    salarioReajustado = salario + (salario * 0.3);
+                    break;
+
+                default:
+                    salarioReajustado = salario + (salario * 0.4);
+                    break;
+            }
+            
+            Console.WriteLine($"Salário antigo: {salario.ToString("c")}\nSalário atual: {salarioReajustado.ToString("c")}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Atividade04/CalculaNota/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaNota
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Calcula nota do aluno---");
+
+            Console.Write("Nome do aluno: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Nota do 1º Bimestre: ");
+            int nota1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nota do 2º Bimestre: ");
+            int nota2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nota do 3º Bimestre: ");
+            int nota3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nota do 4º Bimestre: ");
+            int nota4 = Convert.ToInt32(Console.ReadLine());
+
+            double media = (nota1 + nota2 + nota3 + nota4)/ 4;
+            char conceito = ' ';
+            string situacao;
+
+            if (media >= 90)
+            {
+                conceito = 'A';
+                
+            }
+            else if (media >= 80)
+            {
+                conceito = 'B';
+                
+            }
+            else if (media >= 60)
+            {
+                conceito = 'C';
+               
+            }
+            else if (media >= 40)
+            {
+                conceito = 'D';
+                    
+            }
+            else if (media >= 20)
+            {
+                conceito = 'E';
+                
+            }
+
+
+            switch (conceito)
+            {
+                case 'A':
+                    situacao = "Aprovado";
+                    break;
+                case 'B':
+                    situacao = "Aprovado";
+                    break;
+                case 'C': 
+                    situacao = "Aprovado";
+                    break;
+
+                case 'D':
+                    situacao = "Recuperação";
+                    break;
+
+                case 'E':
+                    situacao = "Exame";
+                    break;
+                    
+                default:
+                    situacao = "Reprovado";
+                    break;
+                
+            }
+            Console.WriteLine($"Olá {nome}, você ganhou {conceito} e está {situacao}");
+            
+
+
+
+
+        }
+    }
+}
