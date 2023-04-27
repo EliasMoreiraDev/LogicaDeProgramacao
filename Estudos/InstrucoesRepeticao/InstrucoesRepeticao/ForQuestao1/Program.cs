61a9abb5310d9c3dc77ea7a945371b54f0d79b49
2022103030076 <elias.moreiradosantos.28@gmail.com>
2023-04-27 15:14:28 -0400

    asas

---
+++ Estudos/InstrucoesRepeticao/InstrucoesRepeticao/ForQuestao1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForQuestao1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("\t\t---Realjuste Salarial---");
+                Console.Write($"\nSalário do {i}º funcionário: ");
+                double salario = double.Parse(Console.ReadLine());
+                
+
+                if (salario < 1000)
+                {
+                    double reajuste = salario * 0.55;
+                    double novosalario = salario + reajuste;
+                    Console.WriteLine("\n\t\t---Reajuste---"); 
+                    Console.WriteLine($"Salario atual: {salario:c2}");
+                    Console.WriteLine($"Reajuste: {reajuste:c2}");
+                    Console.WriteLine($"Novo Salario: {novosalario:c2}");
+                
+                }
+                else if(salario < 2500)
+                {
+                    double reajuste = salario * 0.30;
+                    double novosalario = salario + reajuste;
+                    Console.WriteLine("\n\t\t---Reajuste---");
+                    Console.WriteLine($"Salario atual: {salario:c2}");
+                    Console.WriteLine($"Reajuste: {reajuste:c2}");
+                    Console.WriteLine($"Novo Salario: {novosalario:c2}");
+                
+                }
+                else
+                {
+                    double reajuste = salario * 0.20;
+                    double novosalario = salario + reajuste;
+                    Console.WriteLine("\n\t\t---Reajuste---");
+                    Console.WriteLine($"Salario atual: {salario:c2}");
+                    Console.WriteLine($"Reajuste: {reajuste:c2}");
+                    Console.WriteLine($"Novo Salario: {novosalario:c2}");
+                
+                }
+               Console.ReadKey(); 
+            }
+            
+        }
+    }
+}
---
+++ Estudos/InstrucoesRepeticao/InstrucoesRepeticao/ForQuestao2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForQuestao2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\t\t---Converte Celsius e Fahrenheit---");
+            Console.Write("\nTemperatura mínima em Fahrenheit: ");
+            int valorMinimo = int.Parse(Console.ReadLine());
+            Console.Write("\nTemperatura máxima em Fahrenheit: ");
+            int valorMaximo = int.Parse(Console.ReadLine());
+
+            Console.Write("\nInforme o valor da escala: ");
+            int incremento = int.Parse(Console.ReadLine());
+            Console.Clear();
+            for (; valorMinimo <= valorMaximo; valorMinimo+=incremento)
+            {
+                
+                int c = (5 * (valorMinimo - 32)) / 9;
+                Console.WriteLine($"O valor em Fahrenheit é {valorMinimo}\nValor em Celcius é {c}");
+                Console.WriteLine("-------------------------");
+            }
+            Console.ReadKey();
+        }
+    }
+}
