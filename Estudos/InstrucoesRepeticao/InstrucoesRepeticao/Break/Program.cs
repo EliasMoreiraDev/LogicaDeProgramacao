c5e16eed2ed48d715c6f4f22280b217bc6e4de10
2022103030076 <elias.moreiradosantos.28@gmail.com>
2023-05-09 11:30:28 -0400

    as

---
+++ Estudos/InstrucoesRepeticao/InstrucoesRepeticao/Break/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Instrução Break---");
+
+            int soma = 0;
+            Random random = new Random();
+
+            for (int i = 1; i <=3; i++)
+            {
+                Console.WriteLine("Jogada "+i);
+                int numero = random.Next(1, 6);
+                Console.WriteLine("Número: " + numero);
+                if(numero % 2 != 0)
+                {
+                    continue;
+                }
+                soma += numero;
+                Console.WriteLine("Soma: "+soma);
+                Console.WriteLine("");
+                if (soma > 9)
+                {
+                    Console.WriteLine("Você ganhou com "+ i +" jogada(s)");
+                    break;
+                }
+                
+            }
+            if (soma <= 9)
+            {
+                Console.WriteLine("Você perdeu!");
+               
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Estudos/InstrucoesRepeticao/InstrucoesRepeticao/DesafioBreak/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioBreak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        { 
+            Console.WriteLine("\t\t---Grãos e Xadrez---");
+            decimal graos = 1;
+            
+            int casaXadrez = 1;
+
+            for (; casaXadrez <= 64; casaXadrez++)
+            {
+                decimal kilos = graos / 15432;
+                Console.WriteLine($"Quantidade de graos: {graos:n2}");
+                graos += graos;
+
+                
+                Console.WriteLine($"Quantidade de kilos {kilos:n2}\n");
+            }
+
+
+        }
+    }
+}
