2a3f11ff7fcc37ecd5307d155567eea7ff1c8656
2022103030076 <elias.moreiradosantos.28@gmail.com>
2023-05-23 10:54:37 -0400

    dsadas

---
+++ Estudos/InstrucoesRepeticao/Array/Array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[100];
+
+            /*for (int i = 0; i < 100; i++)
+            {
+                numeros[i] = i * 2;
+                Console.WriteLine(numeros[i]);
+            }*/
+          
+    }
+}
